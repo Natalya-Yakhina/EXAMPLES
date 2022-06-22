b6c27022ef3036a7addc3012820680fae3d28ac1
Яхина Наталья Андреевна <  @vvrz.local>
2022-06-22 15:22:36 +0300

    Example005_ConditionIfElse

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Приветствие по особенному (функция if else)
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
