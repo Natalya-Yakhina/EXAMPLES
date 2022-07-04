0dd1885f0b3a4149ab2ca3ea7a436037bb9363c1
Наталья Яхина <ivanechka93@mail.ru>
2022-07-04 09:46:30 +0300

    014

---
+++ Examples/Example014_FOR/Program.cs
@@ -0,0 +1,26 @@
+﻿// Цикл for - собирает все
+
+string Method4(int count, string text) // строку С компоновать count раз
+{
+    string result = String.Empty; // String.Empty - пустая строка
+    
+    for(int i = 0; i < count; i++ ) // инициализация счетчика ; проверка условия ; счетчик
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+// Цикл в цикле
+// пример таблица умножения
+
+for(int i = 1; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}"); // интерполяция строк
+    }
+    Console.WriteLine(); // переход на новую строку
+}
