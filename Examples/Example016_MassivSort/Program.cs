42df59314a566cd73bf754278af4c60a4f22a703
Наталья Яхина <ivanechka93@mail.ru>
2022-07-04 10:54:58 +0300

    016

---
+++ Examples/Example016_MassivSort/Program.cs
@@ -0,0 +1,46 @@
+﻿// 
+//
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиии
+// 3. Повторять пока есть не отсортированные элементы
+
+int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// вывод массива на экран
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++) // пробегаем по всем элементам и показываем на экране
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+// упорядочивание массива
+void SellectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++) // искуственно отнять, т.к. при поиске минимального приавляем 1
+    {
+        int minPosition = i;
+        
+        // ищем минимальный элемент
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            // здесь ищем мин элемент
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        
+        
+        int temporary = array[i];
+        array[i] = array[minPosition]; // обмен двух переменных местами
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(array);
+SellectionSort(array);
+
+
+PrintArray(array);
+
